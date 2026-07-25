a0488a87bdfe4235074188a4e3acb4d97a892a05
Merve Gazioglu <mervegazioglu10@gmail.com>
2026-07-25 23:06:15 +0300

    refactor(core): move BorrowRequestDto to Library.Core project

---
+++ Library.Core/BorrowRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core
+{
+    // İstek gövdesini (JSON body) düzgün karşılayabilmek için yardımcı DTO sınıfı
+    public class BorrowRequestDto
+    {
+        public int BookId { get; set; }
+        public int MemberId { get; set; }
+        public string CountryCode { get; set; } = string.Empty;
+    }
+}
