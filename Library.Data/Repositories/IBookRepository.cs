3c615e99bc7f6df820bfe95a3edb7690408c8879
Merve Gazioglu <mervegazioglu10@gmail.com>
2026-07-27 16:28:07 +0300

    feat(data): add IBookRepository and BookRepository for data access abstraction

---
+++ Library.Data/Repositories/IBookRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Core;
+
+namespace Library.Data.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<(List<Book> Items, int TotalCount)> GetAllAsync(int page, int pageSize);
+        Task<Book?> GetByIdAsync(int id);
+        Task AddAsync(Book book);
+        Task<bool> UpdateAsync(Book book);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+    }
+}
