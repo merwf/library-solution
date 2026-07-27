953bf72765dd1d05e17562a1cb8b863b3415d830
Merve Gazioglu <mervegazioglu10@gmail.com>
2026-07-27 17:39:09 +0300

    feat(data): add IBorrowRepository and BorrowRepository implementations

---
+++ Library.Data/Repositories/IBorrowRepository.cs
@@ -0,0 +1,17 @@
+﻿using Library.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Data.Repositories
+{
+    public interface IBorrowRepository
+    {
+        Task<BorrowRecord?> GetByIdWithBookAsync(int id);
+        Task<List<BorrowRecordDto>> GetActiveBorrowsAsync();
+        Task AddAsync(BorrowRecord record);
+        Task SaveChangesAsync();
+    }
+}
